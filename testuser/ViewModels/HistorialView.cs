dd6e93e33a2fb646dade907f7d48e8658bdd1baa
Abner Cruz <abnerkruz@gmail.com>
2019-02-18 14:16:56 -0600

    -Se Arregló vistas pendientes -Se agregó un ViewModel

---
+++ testuser/ViewModels/HistorialView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using testuser.Models.SysModel;
+
+namespace testuser.ViewModels
+{
+    public class HistorialView
+    {
+        public tblCasos casos;
+        public tblHistorial historial;
+    }
+}
