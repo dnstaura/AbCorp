0ad62c9938834fd09a666ab2076a4c668c400127
Dennis Taura <xlx_taura_xlx@hotmail.co.uk>
2019-04-01 08:35:46 -0600

    Se agrego completamente notificacion de cumpleaños a todos los modulos

--- testuser/ViewModels/CumpleaniosView.cs
+++ testuser/ViewModels/CumpleaniosView.cs
@@ -8,7 +8,7 @@ namespace testuser.ViewModels
 {
     public class CumpleaniosView
     {
-        public string nombreClienteAF { get; set; }
+        public string nombreCliente { get; set; }
         public int dias { get; set; }
 
 
@@ -43,7 +43,7 @@ namespace testuser.ViewModels
                     {
                         var clienteAF = new CumpleaniosView
                         {
-                            nombreClienteAF = item.nombres + " " + item.apellidos,
+                            nombreCliente = item.nombres + " " + item.apellidos,
                             //dias = faltan.Days
                             //dias = faltan.Days == 0 ? faltan.Days + 1 : faltan.Days
                             dias = DateTime.Now.Day == diaCumple ? 0 : faltan.Days + 1
@@ -52,9 +52,95 @@ namespace testuser.ViewModels
                     }
 
                 }
-
                 return cumpleanieros;
+            }
+        }
 
+        public static List<CumpleaniosView> cumpleanosOT()
+        {
+            List<CumpleaniosView> cumpleanieros = new List<CumpleaniosView>();
+            using (dbModel db = new dbModel())
+            {
+                var clientes = (from n in db.tblPersonal
+                                select n).ToList();
+
+                foreach (var item in clientes)
+                {
+                    int diaCumple = item.Fecha_Nacimiento.Day;
+                    int mesCumple = item.Fecha_Nacimiento.Month;
+                    //int anioCumple = 1994;
+
+                    DateTime proximoCumple;
+
+                    if (DateTime.Now.Month > mesCumple && DateTime.Now.Day > diaCumple)
+                    {
+                        proximoCumple = new DateTime(DateTime.Now.AddYears(1).Year, mesCumple, diaCumple);
+                    }
+                    else
+                    {
+                        proximoCumple = new DateTime(DateTime.Now.Year, mesCumple, diaCumple);
+                    }
+
+                    TimeSpan faltan = proximoCumple.Subtract(DateTime.Now);
+
+                    if (faltan.Days >= 0 && faltan.Days <= 200)
+                    {
+                        var clienteOT = new CumpleaniosView
+                        {
+                            nombreCliente = item.Nombres + " " + item.Apellidos,
+                            //dias = faltan.Days
+                            //dias = faltan.Days == 0 ? faltan.Days + 1 : faltan.Days
+                            dias = DateTime.Now.Day == diaCumple ? 0 : faltan.Days + 1
+                        };
+                        cumpleanieros.Add(clienteOT);
+                    }
+
+                }
+                return cumpleanieros;
+            }
+        }
+
+        public static List<CumpleaniosView> cumpleanosEMP()
+        {
+            List<CumpleaniosView> cumpleanieros = new List<CumpleaniosView>();
+            using (dbModel db = new dbModel())
+            {
+                var clientes = (from n in db.tblOtorgante
+                                select n).ToList();
+
+                foreach (var item in clientes)
+                {
+                    int diaCumple = item.fechanacimiento.Day;
+                    int mesCumple = item.fechanacimiento.Month;
+                    //int anioCumple = 1994;
+
+                    DateTime proximoCumple;
+
+                    if (DateTime.Now.Month > mesCumple && DateTime.Now.Day > diaCumple)
+                    {
+                        proximoCumple = new DateTime(DateTime.Now.AddYears(1).Year, mesCumple, diaCumple);
+                    }
+                    else
+                    {
+                        proximoCumple = new DateTime(DateTime.Now.Year, mesCumple, diaCumple);
+                    }
+
+                    TimeSpan faltan = proximoCumple.Subtract(DateTime.Now);
+
+                    if (faltan.Days >= 0 && faltan.Days <= 200)
+                    {
+                        var clienteOT = new CumpleaniosView
+                        {
+                            nombreCliente = item.apellidos + " " + item.apellidos,
+                            //dias = faltan.Days
+                            //dias = faltan.Days == 0 ? faltan.Days + 1 : faltan.Days
+                            dias = DateTime.Now.Day == diaCumple ? 0 : faltan.Days + 1
+                        };
+                        cumpleanieros.Add(clienteOT);
+                    }
+
+                }
+                return cumpleanieros;
             }
         }
 
